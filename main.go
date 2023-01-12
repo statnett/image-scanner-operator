2451a10c54e2e20d8d8797f79b49ad7571879319
Erik Godding Boye <egboye@gmail.com>
2023-01-12 14:05:04 +0100

    fix

--- main.go
+++ main.go
@@ -45,8 +45,8 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
-	utilruntime.Must(stasv1alpha1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
+	utilruntime.Must(stasv1alpha1.AddToScheme(scheme))
 }
 
 func main() {
