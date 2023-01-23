1a68afdc18f9b2266c0610d0fd517dea2905d35c
Erik Godding Boye <egboye@gmail.com>
2023-01-23 10:24:12 +0100

    revert: "refactor: break up main func (#115)"
    This reverts commit e494eedb596ffcf94b37dcdcd6209f2e00fa8d87.

--- pkg/operator/config.go
+++ pkg/operator/config.go
@@ -3,8 +3,6 @@ package operator
 import (
 	"time"
 
-	"github.com/statnett/controller-runtime-viper/pkg/zap"
-
 	stasv1alpha1 "github.com/statnett/image-scanner-operator/api/stas/v1alpha1"
 )
 
@@ -17,7 +15,6 @@ type Config struct {
 	ScanWorkloadResources []string      `mapstructure:"scan-workload-resources"`
 	TrivyImage            string        `mapstructure:"trivy-image"`
 	TrivyServer           string        `mapstructure:"trivy-server"`
-	Zap                   zap.Options   `mapstructure:"-"`
 }
 
 func (c Config) TimeUntilNextScan(cis *stasv1alpha1.ContainerImageScan) time.Duration {
