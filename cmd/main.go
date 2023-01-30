ee358da26bdbe0c445191776d0b0581d18d60db5
Erik Godding Boye <egboye@gmail.com>
2023-01-30 12:29:29 +0100

    refactor: only exit with error from main (#154)

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"log"
 
 	"github.com/statnett/image-scanner-operator/internal/config"
 	"github.com/statnett/image-scanner-operator/internal/operator"
@@ -12,7 +13,15 @@ func main() {
 	cfg.Zap.Development = true
 
 	opr := operator.Operator{}
-	opr.BindConfig(&cfg, flag.CommandLine)
-	opr.ValidateConfig(cfg)
-	opr.Start(cfg)
+	if err := opr.BindConfig(&cfg, flag.CommandLine); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := opr.ValidateConfig(cfg); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := opr.Start(cfg); err != nil {
+		log.Fatal(err)
+	}
 }
