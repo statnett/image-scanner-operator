9de589d971fec5ea8b38e84e0188f40f172fbc0e
Erik Godding Boye <egboye@gmail.com>
2023-02-24 13:27:58 +0100

    regen

--- internal/controller/stas/scan_job_backoff_pod_controller_test.go
+++ internal/controller/stas/scan_job_backoff_pod_controller_test.go
@@ -3,12 +3,12 @@ package stas
 import (
 	"context"
 	"fmt"
-	"k8s.io/apimachinery/pkg/api/errors"
 	"path"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/envtest/komega"
 
