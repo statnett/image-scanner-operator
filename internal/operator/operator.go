a185ee13f9e9d354f04c722a0378b3a55f3f2572
Erik Godding Boye <egboye@gmail.com>
2023-05-26 20:06:28 +0200

    fix bootstrap

--- internal/operator/operator.go
+++ internal/operator/operator.go
@@ -13,13 +13,16 @@ import (
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 	eventsv1 "k8s.io/api/events/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/kubernetes"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 
@@ -112,16 +115,15 @@ func (o Operator) Start(cfg config.Config) error {
 			Unstructured: true,
 			DisableFor:   []client.Object{&eventsv1.Event{}},
 		}},
-		Scheme:                 scheme,
+		Scheme: scheme,
+		MapperProvider: func(c *rest.Config, httpClient *http.Client) (meta.RESTMapper, error) {
+			return apiutil.NewDiscoveryRESTMapper(c, httpClient)
+		},
+		Cache:                  cache.Options{Namespaces: cfg.ScanNamespaces},
 		MetricsBindAddress:     metricsAddr,
-		Port:                   9443,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "398aa7bc.statnett.no",
-	}
-
-	if len(cfg.ScanNamespaces) > 0 {
-		options.NewCache = cache.MultiNamespacedCacheBuilder(cfg.ScanNamespaces)
 	}
 
 	kubeConfig := ctrl.GetConfigOrDie()
