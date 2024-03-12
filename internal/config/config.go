5e22f149a1b8c67c6196c1abf60e3dba51ad35f6
Erik Godding Boye <egboye@gmail.com>
2024-03-12 20:00:54 +0100

    feat: add config to use rootfs as an alternative to filesystem Trivy scanning

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"errors"
+	"fmt"
 	"regexp"
 	"time"
 )
@@ -15,5 +17,38 @@ type Config struct {
 	ScanNamespaceIncludeRegexp *regexp.Regexp `mapstructure:"scan-namespace-include-regexp"`
 	ScanWorkloadResources      []string       `mapstructure:"scan-workload-resources"`
 	TrivyImage                 string         `mapstructure:"trivy-image"`
+	TrivyCommand               TrivyCommand   `mapstructure:"trivy-command"`
 	ActiveScanJobLimit         int            `mapstructure:"active-scan-job-limit"`
+}
+
+type TrivyCommand string
+
+const (
+	FilesystemTrivyCommand TrivyCommand = "filesystem"
+	RootfsTrivyCommand     TrivyCommand = "rootfs"
+)
+
+var errUnmarshalNilLevel = errors.New("can't unmarshal a nil *TrivyCommand")
+
+// UnmarshalText unmarshals text to a trivy command.
+func (c *TrivyCommand) UnmarshalText(text []byte) error {
+	if c == nil {
+		return errUnmarshalNilLevel
+	}
+	if !c.unmarshalText(text) {
+		return fmt.Errorf("unrecognized trivy command: %q", text)
+	}
+	return nil
+}
+
+func (c *TrivyCommand) unmarshalText(text []byte) bool {
+	switch string(text) {
+	case "filesystem":
+		*c = FilesystemTrivyCommand
+	case "rootfs":
+		*c = RootfsTrivyCommand
+	default:
+		return false
+	}
+	return true
 }
