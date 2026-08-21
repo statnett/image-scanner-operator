0b61b6f72faf4c2d14b80eca7509a65cf43f7bf6
Erik Godding Boye <egboye@gmail.com>
2026-08-21 13:24:23 +0200

    fix: replace use of deprecated Result.Requeue with Result.RequeueAfter (#1836)

--- internal/controller/reconcile.go
+++ internal/controller/reconcile.go
@@ -5,7 +5,6 @@ import (
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	ctrl "sigs.k8s.io/controller-runtime"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
 
 	staserrors "github.com/statnett/image-scanner-operator/internal/errors"
 )
@@ -14,19 +13,5 @@ type ReconcileFn func(context.Context) (ctrl.Result, error)
 
 func Reconcile(ctx context.Context, reconcileFn ReconcileFn) (ctrl.Result, error) {
 	result, err := reconcileFn(ctx)
-	if apierrors.IsConflict(err) {
-		// Resource conflict; requeue the request
-		return ctrl.Result{Requeue: true}, nil
-	}
-
-	if apierrors.IsAlreadyExists(err) {
-		// Log error message as warning (-1 = WARN)
-		logf.FromContext(ctx, "error", err.Error()).
-			V(-1).
-			Info("Assuming transient error (race condition), requeuing request")
-
-		return ctrl.Result{Requeue: true}, nil //nolint:staticcheck // SA1019: FIXME: https://github.com/kubernetes-sigs/controller-runtime/pull/3107#issuecomment-2648121233
-	}
-
 	return result, staserrors.IgnoreAny(err, staserrors.IsNamespaceTerminating, apierrors.IsNotFound)
 }
